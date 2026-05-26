15d816cc66cf71176a03437c038aa91ffecc875c
lksamanta1969 <lk.samanta1969@gmail.com>
2026-05-26 21:19:24 +0530

    Firebase setup updated

--- facelook-app/src/firebase.js
+++ facelook-app/src/firebase.js
@@ -1,16 +1,19 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBrIafM3t1lZeMvu-gi4McPyq0AvCCpnf8",
   authDomain: "facelook-e2717.firebaseapp.com",
   projectId: "facelook-e2717",
-  storageBucket: "facelook-e2717.firebasestorage.app", 
+  storageBucket: "facelook-e2717.firebasestorage.app",
   messagingSenderId: "254789334698",
   appId: "1:254789334698:web:928145f9f83a90f05782d3"
 };
+
 const app = initializeApp(firebaseConfig);
 
+export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
